bcb52acf067047c5d214d6d62a4ee27e0029df07
yinger <75009049+yingerliang@users.noreply.github.com>
2022-03-30 16:16:45 +1300

    Added javascript

---
+++ src/script.js
@@ -0,0 +1,45 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp)
+  let hours = date.getHours()
+  if (hours < 10) {
+    hours = `0${hours}`
+  }
+  let minutes = date.getMinutes()
+  if (minutes < 10) {
+    minutes = `0${minutes}`
+  }
+
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+  let day = days[date.getDay()]
+  return `${day}, ${hours}:${minutes}`
+}
+
+function displayTemperature(response) {
+  console.log(response.data)
+  let temperatureElement = document.querySelector('#temperature')
+  let cityElement = document.querySelector('#city')
+  let descriptionElement = document.querySelector('#description')
+  let humidityElement = document.querySelector('#humidity')
+  let windElement = document.querySelector('#wind')
+  let dateElement = document.querySelector('#date')
+
+  temperatureElement.innerHTML = Math.round(response.data.main.temp)
+  cityElement.innerHTML = response.data.name
+  descriptionElement.innerHTML = response.data.weather[0].description
+  humidityElement.innerHTML = response.data.main.humidity
+  windElement.innerHTML = Math.round(response.data.wind.speed)
+  dateElement.innerHTML = formatDate(response.data.dt * 1000)
+}
+
+let apiKey = 'c02f2f4ad7a3939670c2af0979e7bdd0'
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&units=metric&appid=${apiKey}&units=metric`
+
+axios.get(apiUrl).then(displayTemperature)
