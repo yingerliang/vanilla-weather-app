6fd7e27a9d01faff0bf6b131a9fc452bc1b858c6
yinger <75009049+yingerliang@users.noreply.github.com>
2022-03-30 16:32:10 +1300

    Added relevant weather icon

--- src/script.js
+++ src/script.js
@@ -23,13 +23,13 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  console.log(response.data)
   let temperatureElement = document.querySelector('#temperature')
   let cityElement = document.querySelector('#city')
   let descriptionElement = document.querySelector('#description')
   let humidityElement = document.querySelector('#humidity')
   let windElement = document.querySelector('#wind')
   let dateElement = document.querySelector('#date')
+  let iconElement = document.querySelector('#icon')
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp)
   cityElement.innerHTML = response.data.name
@@ -37,6 +37,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.main.humidity
   windElement.innerHTML = Math.round(response.data.wind.speed)
   dateElement.innerHTML = formatDate(response.data.dt * 1000)
+  iconElement.setAttribute(
+    'src',
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+  )
+  iconElement.setAttribute('alt', response.data.weather[0].description)
 }
 
 let apiKey = 'c02f2f4ad7a3939670c2af0979e7bdd0'
