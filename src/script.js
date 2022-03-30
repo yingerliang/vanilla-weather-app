8b5bafb3277940dc916836104d63cf9543067072
yinger <75009049+yingerliang@users.noreply.github.com>
2022-03-30 18:44:59 +1300

    Removed unit conversion

--- src/script.js
+++ src/script.js
@@ -111,32 +111,7 @@ function handleSubmit(event) {
   search(cityInputElement.value)
 }
 
-// Temperature Converter
-function convertFahrenheitTemperature(event) {
-  event.preventDefault()
-  let temperatureElement = document.querySelector('#temperature')
-  celsiusLink.classList.remove('active')
-  fahrenheitLink.classList.add('active')
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature)
-}
-function convertCelsiusTemperature(event) {
-  event.preventDefault()
-  let temperatureElement = document.querySelector('#temperature')
-  celsiusLink.classList.add('active')
-  fahrenheitLink.classList.remove('active')
-  temperatureElement.innerHTML = Math.round(celsiusTemperature)
-}
-
-let celsiusTemperature = null
-
 let form = document.querySelector('#search-form')
 form.addEventListener('submit', handleSubmit)
-
-let fahrenheitLink = document.querySelector('#fahrenheit')
-fahrenheitLink.addEventListener('click', convertFahrenheitTemperature)
-
-let celsiusLink = document.querySelector('#celsius')
-celsiusLink.addEventListener('click', convertCelsiusTemperature)
 
 search('New York')
